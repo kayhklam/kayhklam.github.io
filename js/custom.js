6c078c8cf26c8b6d3a95457e4f2b042b04e35b68
kayhklam <kay.96.lam@gmail.com>
2019-07-15 16:55:37 +0100

    Insta changes

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         get: 'user',
         userId: '10983788711',
         limit: 6,
-        resolution: 'low_resolution',
+        resolution: 'standard_resolution',
         accessToken: '10983788711.1677ed0.045fc628888443fa886dd783a4322315',
         sortBy: 'most-recent',
         template: '<div class="col-lg-1 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
