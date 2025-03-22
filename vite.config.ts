efb4a65b1bda23c1a256377800106c17729329c5
kayhklam <52869985+kayhklam@users.noreply.github.com>
2025-03-22 23:40:24 +0000

    add image optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -11,8 +12,10 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
+    ViteImageOptimizer({
+      jpeg: { quality: 80 }
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
